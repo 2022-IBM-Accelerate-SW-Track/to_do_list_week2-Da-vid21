907ec60032a6aaf103c7b606a5f2d8d127d5ee41
David Tekeste <esubalewdavid@gmail.com>
2022-06-16 18:07:27 -0400

    Done till part 3

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic_name from "../assets/profile_pic_name.png";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
+        <div class="about">
+        <p> <about>About</about><b>   me</b> </p> 
+        </div>
+        <div>
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src={profile_pic_name}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Dawit Tekeste</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              2022 IBM Accelerate Software Track attendee
             </div>
           </div>
         </div>
       </div>
-    );
+      </div>
+      
+      
+    )
+    
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,6 +27,14 @@ class Home extends Component {
       todos: new_list,
     });
   };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  };  
   render() {
     return (
       <div className="Home">
@@ -34,9 +42,10 @@ class Home extends Component {
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
+        
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos}  deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
